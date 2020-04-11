cef41827759c04764b1a394adefbb5c4a4c60f78
vkan <vkanstudy@gmail.com>
2020-04-11 04:49:51 +0300

    init commit

--- 28_03/fatoi/main.c
+++ 28_03/fatoi/main.c
@@ -2,26 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 
-/* 3) Реализовать функцию int fatoi (const char *str) для приведения (конвертации) строки в числовой вид.*/
-
 void fatoi (const char *str)
 {
     int i;
-
     for(i=0;i<strlen(str);i++)
-    {
         printf("%d\n", (int)str[i]);
-    }
 }
-
 
 int main()
 {
     char s[1000];
 
     scanf("%s", s);
-
     fatoi(s);
-    printf("Hello world!\n");
     return 0;
 }
